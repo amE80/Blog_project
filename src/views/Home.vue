61fefb2ce76cdfcaf2047a6e2294ccd64b76f2fb
hhmehsan <hhmehsan80@gmail.com>
2023-02-09 21:24:48 +0330

    header created in a simple way

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template> <div>
         <TopNav />
+        <Header />
 </div>
 </template>
 
 <script>
+import Header from "../components/header.vue"
 import TopNav from "../components/topNav.vue";
 export default{
 components:{
     TopNav,
+    Header,
 }
 }
 </script>
