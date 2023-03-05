721e85452834d6315193bce077e65b83430ab06c
hhmehsan <hhmehsan80@gmail.com>
2023-03-05 10:57:18 +0330

    color fixed to use default and custom together

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 
-// const {colors:defaultColors} = require('tailwindcss/defaultTheme')
-// const colors ={
-//   ...defaultColors,
-//   ...{
-//     "custom-yellow":{
-//       "500": "#edae0a",
-//     },
-//   },
-// }
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js,vue}"],
   theme: {
@@ -18,6 +10,16 @@ module.exports = {
         main: ["OpenSans"],
         mainB: ["OpenSansB"]
       },
+      colors: {
+        'brwn': "#800000",
+        'crm': "#ffebcd",
+        'gray': "rgba(0,0,0,0.6)",
+        'lightGray': "rgba(0,0,0,0.3)",
+        'red' : "#880808",
+        'black' : '#000',
+        'white' : '#fff',
+      
+        },
     },
     debugScreens: {
       position: ["top", "left"],
@@ -27,15 +29,7 @@ module.exports = {
       "4xl":
         " inset 0 8px 8px -8px rgba(0, 0, 0,0.7) , inset 0 -8px 8px -8px rgba(0, 0, 0,0.7)",
     },
-    colors: {
-      brwn: "#800000",
-      crm: "#ffebcd",
-      gray: "rgba(0,0,0,0.6)",
-      lightGray: "rgba(0,0,0,0.3)",
-      red : "#880808",
-      black : '#000',
-      white : '#fff',
-    },
+   
   },
   plugins: [require("tailwindcss-debug-screens")],
 };
