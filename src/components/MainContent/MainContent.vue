874a421d6545971e3930c78628d91f0cd220ac95
hhmehsan <hhmehsan80@gmail.com>
2023-03-05 11:17:07 +0330

    profile image , like button actions , use article instead new data

--- src/components/MainContent/MainContent.vue
+++ src/components/MainContent/MainContent.vue
@@ -8,23 +8,30 @@
       :key="article.id">
       <div class="flex justify-between">
         <div class="flex items-center mt-5 content">
-          <div class="rounded-full w-11 h-11 bg-brwn">
-            <UserIcon />
-
-          </div>
+        <img :src='article.author.image' class="rounded-full w-11 h-11" alt="pic">
           <section v-if="article.author" class="ml-2">
             <p class="text-sm">{{ article.author.username }}</p>
             <p class="text-sm text-gray">{{ article.createdAt }}</p>
           </section>
         </div>
-        <button :class="likeBeRed" @click="likeCounter"
-          class="flex cursor-pointer items-center justify-center w-auto h-10 px-2 mt-5 border-2 rounded text-red border-red hover:bg-lightGray">
-          <label class="inline cursor-pointer max-h-full max-w-full" for="like">
+
+        <button  v-if="article.favorited == true"
+        @click="articleStore.toggleFav(article.slug)"
+          class="flex cursor-pointer items-center justify-center w-auto h-10 px-2 mt-5 border-2 rounded text-crm border-red bg-red hover:bg-lightGray">
             <span> {{ article.favoritesCount }} </span>
-            <heart-icon class="inline" />
-          </label>
-        </button>
-        <input type="checkbox" name="like" id="like" v-model="isLiked" class="hidden">
+            <heart-icon />
+          </button>
+        
+        <button  v-else
+        @click="articleStore.toggleFav(article.slug)"
+          class="flex cursor-pointer items-center justify-center w-auto h-10 px-2 mt-5 border-2 rounded  text-red border-red hover:bg-lightGray">
+            <span> {{ article.favoritesCount }} </span>
+            <heart-icon />
+          </button>
+
+       
+        
+        
       </div>
       <div class="w-11/12 mt-4">
         <p class="">{{ article.title }}</p>
@@ -61,8 +68,7 @@ export default {
     return {
       artTime: null,
       articles: null,
-      isLiked: false,
-      isLoading: true
+      isLiked: false, 
     };
   },
 
@@ -70,24 +76,9 @@ export default {
     this.articleStore.getPosts();
   },
 
-  computed: {
-    likeBeRed() {
-      return { red: this.isLiked };
-    },
-
-    likeCounter() {
-      console.log('mewo');
-      axios.post('https://api.realworld.io/api/articles/:slug/favorite').then((response) => {
-
-        console.log(response.data.articles);
-      })
-        .catch((error) => {
-          console.log(error);
-        })
-    },
 
   }
-}
+
 </script>
 
 <style scoped>
