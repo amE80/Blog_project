aa770b130d9999f1a9df5ca8bc803095b654e2f1
hhmehsan <hhmehsan80@gmail.com>
2023-03-05 11:14:08 +0330

    title goes home page

--- src/components/Navigation/TopNav.vue
+++ src/components/Navigation/TopNav.vue
@@ -1,7 +1,7 @@
 <template>
      <nav class="my-3 mx-7 md:mx-20 ">
         <ul class="flex items-center justify-between">
-             <li class="text-2xl font-bold md:text-3xl text-brwn "> <h2> Blog Hub </h2></li>
+             <li class="text-2xl font-bold md:text-3xl text-brwn "> <h2> <router-link :to="{name: 'home'}"> Blog Hub </router-link> </h2></li>
            <ul class="flex items-center justify-between w-40 sm:w-48">      
             <li  class="text-sm cursor-pointer md:text-base hover:text-brwn font-main text-lightGray"><router-link active-class="text-brwn" :to="{name: 'home'}"> Home </router-link></li>
             <li  class="text-sm cursor-pointer md:text-base hover:text-brwn font-main text-lightGray" ><router-link :to="{name: 'signIn'}" active-class="text-brwn"> Sign in </router-link> </li>
